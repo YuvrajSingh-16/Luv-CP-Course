8f23db45a791eac407f2d06a813e12600da7157d
Yuvraj Singh Kiraula <yuvrajsinghk1602@gmail.com>
2022-06-30 21:53:10 +0530

    added DFS precomputation and tree

---
+++ Graphs & Trees/DFS Precomputation.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define lli long long int
+
+const int N = 1e5+10;
+vector<int> graph[N];
+
+int subtree_sum[N];
+int even_ct[N];
+
+
+/** Given Q Queries, Q <= 10^5
+ * In each query given V,
+ * Print subtree sum of V & Number of even numbers
+ * in subtree of V
+**/
+
+
+// // Input
+// 13
+// 1 2
+// 1 3
+// 1 13
+// 2 5
+// 3 4
+// 5 6
+// 5 7
+// 5 8
+// 8 12
+// 4 9
+// 4 10
+// 10 11
+
+void dfs(int vertex, int parent=0){
+	/**
+	* Take action on vertex after entering the vertex
+	**/ 
+
+	if(vertex % 2 == 0)
+		even_ct[vertex]++;
+
+	subtree_sum[vertex] += vertex;
+	for(int child: graph[vertex]){
+		if(child == parent) continue;
+		/**
+		 * Take action on child before entering the child node
+		 **/
+
+		dfs(child, vertex);
+		subtree_sum[vertex] += subtree_sum[child];
+		even_ct[vertex] += even_ct[child];
+
+		/** 
+		 * Take action on child after exiting the child node
+		 **/
+	}
+	/** 
+	 * Take action on vertex before exiting the vertex
+	 **/
+}
+// TC: O(V+2*E)
+
+
+int main(){
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+
+		// Vertices -> n, Edges -> n-1
+		int n;
+		cin>>n;
+
+		for(int i=0; i<n-1; i++){
+			int v1, v2;
+			cin>>v1>>v2;
+
+			graph[v1].push_back(v2);
+			graph[v2].push_back(v1);
+		}
+
+		dfs(1);
+
+		for(int i=1; i<=n; i++){
+			cout<<subtree_sum[i]<<" "<<even_ct[i]<<endl;
+		}
+
+		// 	int q;
+		// cin>>q;
+
+		// while(q--){
+		// 	int v;
+		// 	cin>>v;
+		// 	cout<<subtree_sum[v]<<" "<<even_ct[v]<<endl;
+		// }
+
+	return 0;
+}
---
+++ Graphs & Trees/DFS Tree.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define lli long long int
+
+const int N = 1e5+10;
+vector<int> graph[N];
+
+int depth[N];
+int height[N];
+
+// // Input
+// 6 9
+// 1 3 
+// 1 5 
+// 3 5  
+// 3 4 
+// 3 6 
+// 3 2 
+// 2 6 
+// 4 6 
+// 5 6 
+
+void dfs(int vertex, int parent=0){
+	/**
+	* Take action on vertex after entering the vertex
+	**/ 
+	for(int child: graph[vertex]){
+		if(child == parent) continue;
+		/**
+		 * Take action on child before entering the child node
+		 **/
+		depth[child] = depth[vertex]+1;
+		dfs(child, vertex);
+		height[vertex] = max(height[vertex], height[child]+1);
+		/** 
+		 * Take action on child after exiting the child node
+		 **/
+	}
+	/** 
+	 * Take action on vertex before exiting the vertex
+	 **/
+}
+// TC: O(V+2*E)
+
+
+int main(){
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+
+		// Vertices -> n, Edges -> n-1
+		int n;
+		cin>>n;
+
+		for(int i=0; i<n-1; i++){
+			int v1, v2;
+			cin>>v1>>v2;
+
+			graph[v1].push_back(v2);
+			graph[v2].push_back(v1);
+		}
+
+		dfs(1);
+
+		for(int i=1; i<=n; i++){
+			cout<<depth[i]<<" "<<height[i]<<endl;
+		}
+
+	return 0;
+}
